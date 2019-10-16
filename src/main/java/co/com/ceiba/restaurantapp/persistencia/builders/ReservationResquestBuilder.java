b8d78beee193b3d9bfcfc4a948942d1db5f37b55
david <juliandmarias@gmail.com>
2019-10-16 16:33:34 -0500

    cambios nombres

--- src/main/java/co/com/ceiba/restaurantapp/persistencia/builders/ReservationResquestBuilder.java
+++ src/main/java/co/com/ceiba/restaurantapp/persistencia/builders/ReservationResquestBuilder.java
@@ -15,7 +15,7 @@ import co.com.ceiba.restaurantapp.dto.ReservationRequest;
 @Configuration
 public class ReservationResquestBuilder {
 
-	Calendar CurrentDate =  Calendar.getInstance();
+	Calendar currentDate =  Calendar.getInstance();
 
 	private static final int PERCENT_DAYS = 20;
 	private static final int PERCENT_FOR_PEOPLE = 15;
@@ -158,7 +158,7 @@ public class ReservationResquestBuilder {
 	public long differenceBetweenCurrentDateAndReservationDate(ReservationRequest reservationRequest) {
 		long daysDifference = 0;
 		Date fechaEntrada =reservationRequest.getReservationDate().getTime();
-		Date fechaHoy = CurrentDate.getTime();
+		Date fechaHoy = currentDate.getTime();
 		daysDifference = (fechaEntrada.getTime() - fechaHoy.getTime()) / 86400000;
 		return daysDifference;
 	}
