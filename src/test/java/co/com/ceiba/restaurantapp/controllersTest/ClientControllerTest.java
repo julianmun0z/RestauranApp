25d39de0dd2e0d253e0fb7f4b7a1f0beba5111c4
david <juliandmarias@gmail.com>
2019-10-21 08:32:14 -0500

    usando componenscam

--- src/test/java/co/com/ceiba/restaurantapp/controllersTest/ClientControllerTest.java
+++ src/test/java/co/com/ceiba/restaurantapp/controllersTest/ClientControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ComponentScan("co.com.ceiba")
 public class ClientControllerTest {
 
 	
