9353ac0371ca016d019fd342fb85e5c0b5b4bcaa
david <juliandmarias@gmail.com>
2019-10-21 09:57:22 -0500

    cambios test

---
+++ src/test/java/co/com/ceiba/restaurantapp/RestaurantappApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.ceiba.restaurantapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
