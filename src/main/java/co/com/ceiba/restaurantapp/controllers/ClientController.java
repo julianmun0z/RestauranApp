ac9e6d9f4fd600b1f86ec93533b7ad0f7e0a6294
david <juliandmarias@gmail.com>
2019-10-21 16:18:32 -0500

    prueba

--- src/main/java/co/com/ceiba/restaurantapp/controllers/ClientController.java
+++ src/main/java/co/com/ceiba/restaurantapp/controllers/ClientController.java
@@ -29,10 +29,6 @@ public class ClientController {
 		return clientService.getClients(); 
 	}
 
-	@PostMapping
-	public void add(@RequestBody Client client) {
-		clientService.addClient(client);
-	}
 
 	@GetMapping(path = { "/{id}" })
 	public Client getClientForId(@PathVariable("id") int id) {
