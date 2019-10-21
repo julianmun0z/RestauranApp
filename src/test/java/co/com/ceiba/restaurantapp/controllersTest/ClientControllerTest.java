0a14a4c353d32e7d2b34e58e37ea81898065e0ee
david <juliandmarias@gmail.com>
2019-10-21 08:24:02 -0500

    intento jupiter api

--- src/test/java/co/com/ceiba/restaurantapp/controllersTest/ClientControllerTest.java
+++ src/test/java/co/com/ceiba/restaurantapp/controllersTest/ClientControllerTest.java
@@ -10,14 +10,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import co.com.ceiba.restaurantapp.controllers.ClientController;
+;
 
 
 
