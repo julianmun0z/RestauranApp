4976da81f32a4d3feeda0ef833ee89fcc05179a2
david <juliandmarias@gmail.com>
2019-10-22 11:04:14 -0500

    que pasa

--- src/test/java/co/com/ceiba/restaurantapp/controllersTest/ReservationRequestControllerTest.java
+++ src/test/java/co/com/ceiba/restaurantapp/controllersTest/ReservationRequestControllerTest.java
@@ -5,12 +5,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,7 +15,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import co.com.ceiba.restaurantapp.TestDataBuilder.ReservationRequestTestDataBuilder;
-import co.com.ceiba.restaurantapp.controllers.ClientController;
 import co.com.ceiba.restaurantapp.controllers.ReservationRequestController;
 import co.com.ceiba.restaurantapp.dto.ReservationRequest;
 
